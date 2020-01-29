29e3b608d3419136ab64a9fdd189478fe2532e50
Alexander Klee <aklee@pdc-online.com>
2020-01-29 15:01:57 +0100

    add upload.php

--- index.php
+++ index.php
@@ -1,42 +1,21 @@
-<?php // read the .ini file and create an associative array
 
-    $db = parse_ini_file("db/config.ini");
-    /* now we can use the info in that file to create the appropriate, string connection, based on type, or just using the other info */
-    $username = $db['username'];
-    $passwd = $db['password'];
-    $dbname = $db['dbname'];
-    $host = $db['host'];
-    $type = $db['type'];
-?>
 
 <html>
   <head>
+  <title>Basic Archive</title>
     <meta charset="utf-8">
-    <meta name="viewport" content="width=device-width">
-    <title>Basic Archive</title>
+    <meta name="viewport" content="width=device-width, initial-scale=1">
     <link href="style.css" rel="stylesheet" type="text/css" />
+    <link rel="stylesheet" href="https://maxcdn.bootstrapcdn.com/bootstrap/4.4.1/css/bootstrap.min.css">
+    <script src="https://ajax.googleapis.com/ajax/libs/jquery/3.4.1/jquery.min.js"></script>
+    <script src="https://cdnjs.cloudflare.com/ajax/libs/popper.js/1.16.0/umd/popper.min.js"></script>
+    <script src="https://maxcdn.bootstrapcdn.com/bootstrap/4.4.1/js/bootstrap.min.js"></script>
   </head>
   <body>
-    <?php echo '<p>Hello World</p>'; ?>
-    <dl>
-    <?php 
-      foreach ($db as $key => $value) {
-          // $arr[3] wird mit jedem Wert von $arr aktualisiert...
-          echo "<dt>{$key}</dt>";
-          echo "<dd>{$value}</dd>";
-      }
-      
-      try {
-        $conn = new PDO("mysql:host={$host};dbname={$dbname}", $username, $password);
-        // set the PDO error mode to exception
-        $conn->setAttribute(PDO::ATTR_ERRMODE, PDO::ERRMODE_EXCEPTION);
-        echo "Connected successfully";
-      }
-      catch(PDOException $e)
-      {
-        echo "Connection failed: " . $e->getMessage();
-      }
-    ?>
-    <dl>
+    <form action="upload.php" method="post" enctype="multipart/form-data">
+        Select image to upload:
+        <input type="file" name="fileToUpload" id="fileToUpload">
+        <input type="submit" value="Upload Image" name="submit">
+    </form>
   </body>
 </html>
