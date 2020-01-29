57ce81359c9069920dd3abbab7965615ae6020ec
ewihutes <ewihutes@s14.server.hostpoint.ch>
2020-01-29 14:19:03 +0100

    fix typo in index.php

--- index.php
+++ index.php
@@ -27,7 +27,7 @@
       }
       
       try {
-        $conn = new PDO("mysql:host={$host};dbname={$dbname}", $username, $password);
+        $conn = new PDO("mysql:host={$host};dbname={$dbname}", $username, $passwd);
         // set the PDO error mode to exception
         $conn->setAttribute(PDO::ATTR_ERRMODE, PDO::ERRMODE_EXCEPTION);
         echo "Connected successfully";
