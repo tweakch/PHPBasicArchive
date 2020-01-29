b659a1f9487ff181d446aa686e522655d26d35b0
Alexander Klee <aklee@pdc-online.com>
2020-01-29 14:04:05 +0100

    read pdo connection string from ini

--- index.php
+++ index.php
@@ -27,7 +27,7 @@
       }
       
       try {
-        $conn = new PDO("mysql:host=$servername;dbname=myDB", $username, $password);
+        $conn = new PDO("mysql:host={$host};dbname={$dbname}", $username, $password);
         // set the PDO error mode to exception
         $conn->setAttribute(PDO::ATTR_ERRMODE, PDO::ERRMODE_EXCEPTION);
         echo "Connected successfully";
