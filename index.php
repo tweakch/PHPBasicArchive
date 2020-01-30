5e239b05ee3eb20072d03b08463e704eddffcb85
Alexander Klee <aklee@pdc-online.com>
2020-01-30 13:11:34 +0100

    add description

--- index.php
+++ index.php
@@ -12,10 +12,21 @@
     <script src="https://maxcdn.bootstrapcdn.com/bootstrap/4.4.1/js/bootstrap.min.js"></script>
   </head>
   <body>
-    <form action="upload.php" method="post" enctype="multipart/form-data">
-        Select image to upload:
-        <input type="file" name="fileToUpload" id="fileToUpload">
-        <input type="submit" value="Upload Image" name="submit">
-    </form>
+    <div class="jumbotron jumbotron-fluid">
+      <div class="container">
+          <h1 class="display-4">PHPBasicArchive</h1>
+          <p class="lead">A simple archive in PHP.</p>
+          <p>PHPBasicArchive is a sample project by <a href="https://github.com/tweakch">tweakch</a>. Check out the source code on <a href="https://github.com/tweakch/PHPBasicArchive">github</a></p>
+      </div>
+    </div>
+    <div class="container">
+      <div class="row">
+        <form action="upload.php" method="post" enctype="multipart/form-data">
+            Select image to upload:
+            <input type="file" name="fileToUpload" id="fileToUpload">
+            <input type="submit" value="Upload Image" name="submit">
+        </form>
+      </div>
+    </div>
   </body>
 </html>
