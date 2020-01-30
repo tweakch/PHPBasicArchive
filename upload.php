42a0086fdb4bcfb4f3edf8e714440f15f55c1f06
Alexander Klee <aklee@pdc-online.com>
2020-01-30 10:26:55 +0100

    update upload.php

--- upload.php
+++ upload.php
@@ -1,62 +1,38 @@
-<?php // read the .ini file and create an associative array
 
-    $db = parse_ini_file("db/config.ini");
-    /* now we can use the info in that file to create the appropriate, string connection, based on type, or just using the other info */
-    $username = $db['username'];
-    $passwd = $db['password'];
-    $dbname = $db['dbname'];
-    $host = $db['host'];
-    $type = $db['type'];
+<html>
+  <head>
+    <title>Basic Archive</title>
+    <meta charset="utf-8">
+    <meta name="viewport" content="width=device-width, initial-scale=1">
+    <link href="style.css" rel="stylesheet" type="text/css" />
+    <link rel="stylesheet" href="https://maxcdn.bootstrapcdn.com/bootstrap/4.4.1/css/bootstrap.min.css">
+    <script src="https://ajax.googleapis.com/ajax/libs/jquery/3.4.1/jquery.min.js"></script>
+    <script src="https://cdnjs.cloudflare.com/ajax/libs/popper.js/1.16.0/umd/popper.min.js"></script>
+    <script src="https://maxcdn.bootstrapcdn.com/bootstrap/4.4.1/js/bootstrap.min.js"></script>
+  </head>
+  <body>
 
-    $target_dir = "uploads/";
-    $target_file = $target_dir . basename($_FILES["fileToUpload"]["name"]);
-    $uploadOk = 1;
-    $imageFileType = strtolower(pathinfo($target_file,PATHINFO_EXTENSION));
-    // Check if image file is a actual image or fake image
-    if(isset($_POST["submit"])) {
-        $check = getimagesize($_FILES["fileToUpload"]["tmp_name"]);
-        if($check !== false) {
-            echo "File is an image - " . $check["mime"] . ".";
-            $uploadOk = 1;
-        } else {
-            echo "File is not an image.";
-            $uploadOk = 0;
+    <?php // read the .ini file and create an associative array
+        include 'lib/tweakch/archive/database_archive.php';
+        include 'lib/tweakch/archive/filesystem_archive.php';
+
+        $db = parse_ini_file("db/config.ini");
+        /* now we can use the info in that file to create the appropriate, string connection, based on type, or just using the other info */
+        $username = $db['username'];
+        $passwd = $db['password'];
+        $dbname = $db['dbname'];
+        $host = $db['host'];
+        $type = $db['type'];
+
+        $archive = new FileSystemArchive("config.ini");
+        try
+        {
+            $path = $archive->add_file($_FILES["fileToUpload"]);
+        } 
+        catch(PDOException $e) 
+        {
+            echo "Sorry, the database connection failed: " . $e->getMessage();
         }
-    }
-    // Check if file already exists
-    if (file_exists($target_file)) {
-        echo "Sorry, file already exists.";
-        $uploadOk = 0;
-    }
-    // Check file size
-    if ($_FILES["fileToUpload"]["size"] > 500000) {
-        echo "Sorry, your file is too large.";
-        $uploadOk = 0;
-    }
-    // Allow certain file formats
-    if($imageFileType != "jpg" && $imageFileType != "png" && $imageFileType != "jpeg"
-    && $imageFileType != "gif" ) {
-        echo "Sorry, only JPG, JPEG, PNG & GIF files are allowed.";
-        $uploadOk = 0;
-    }
-    // Check if $uploadOk is set to 0 by an error
-    if ($uploadOk == 0) {
-        echo "Sorry, your file was not uploaded.";
-    // if everything is ok, try to upload file
-    } else {
-        if (move_uploaded_file($_FILES["fileToUpload"]["tmp_name"], $target_file)) {
-            try {
-                $conn = new PDO("mysql:host={$host};dbname={$dbname}", $username, $passwd);
-                // set the PDO error mode to exception
-                $conn->setAttribute(PDO::ATTR_ERRMODE, PDO::ERRMODE_EXCEPTION);
-                echo "The file ". basename( $_FILES["fileToUpload"]["name"]). " has been uploaded.";
-              }
-              catch(PDOException $e)
-              {
-                echo "Sorry, the database connection failed: " . $e->getMessage();
-              }
-        } else {
-            echo "Sorry, there was an error uploading your file.";
-        }
-    }
-?>
+    ?> 
+    </body>
+</html>
