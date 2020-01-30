911a959a04c57c31b5cf346087f799b0e5d809e8
Alexander Klee <aklee@pdc-online.com>
2020-01-30 10:37:50 +0100

    add jumbotron to upload.php

--- upload.php
+++ upload.php
@@ -1,38 +1,41 @@
 
 <html>
-  <head>
-    <title>Basic Archive</title>
-    <meta charset="utf-8">
-    <meta name="viewport" content="width=device-width, initial-scale=1">
-    <link href="style.css" rel="stylesheet" type="text/css" />
-    <link rel="stylesheet" href="https://maxcdn.bootstrapcdn.com/bootstrap/4.4.1/css/bootstrap.min.css">
-    <script src="https://ajax.googleapis.com/ajax/libs/jquery/3.4.1/jquery.min.js"></script>
-    <script src="https://cdnjs.cloudflare.com/ajax/libs/popper.js/1.16.0/umd/popper.min.js"></script>
-    <script src="https://maxcdn.bootstrapcdn.com/bootstrap/4.4.1/js/bootstrap.min.js"></script>
-  </head>
-  <body>
+    <head>
+        <title>Basic Archive</title>
+        <meta charset="utf-8">
+        <meta name="viewport" content="width=device-width, initial-scale=1">
+        <link href="style.css" rel="stylesheet" type="text/css" />
+        <link rel="stylesheet" href="https://maxcdn.bootstrapcdn.com/bootstrap/4.4.1/css/bootstrap.min.css">
+        <script src="https://ajax.googleapis.com/ajax/libs/jquery/3.4.1/jquery.min.js"></script>
+        <script src="https://cdnjs.cloudflare.com/ajax/libs/popper.js/1.16.0/umd/popper.min.js"></script>
+        <script src="https://maxcdn.bootstrapcdn.com/bootstrap/4.4.1/js/bootstrap.min.js"></script>
+    </head>
+    <body>
 
-    <?php // read the .ini file and create an associative array
-        include 'lib/tweakch/archive/database_archive.php';
-        include 'lib/tweakch/archive/filesystem_archive.php';
+        <div class="jumbotron jumbotron-fluid">
+            <div class="container">
+                <?php // read the .ini file and create an associative array
+                    include 'lib/tweakch/archive/database_archive.php';
+                    include 'lib/tweakch/archive/filesystem_archive.php';
 
-        $db = parse_ini_file("db/config.ini");
-        /* now we can use the info in that file to create the appropriate, string connection, based on type, or just using the other info */
-        $username = $db['username'];
-        $passwd = $db['password'];
-        $dbname = $db['dbname'];
-        $host = $db['host'];
-        $type = $db['type'];
-
-        $archive = new FileSystemArchive("config.ini");
-        try
-        {
-            $path = $archive->add_file($_FILES["fileToUpload"]);
-        } 
-        catch(PDOException $e) 
-        {
-            echo "Sorry, the database connection failed: " . $e->getMessage();
-        }
-    ?> 
+                    $archive = new FileSystemArchive("config.ini");
+                    $header = "header";
+                    $message = "message";
+                    try
+                    {
+                        $path = $archive->add_file($_FILES["fileToUpload"]);
+                        $header = "File uploaded";
+                        $message = $path;
+                    } 
+                    catch(PDOException $e) 
+                    {
+                        $title = "an error occurred";
+                        $message = "Sorry, the database connection failed: " . $e->getMessage();
+                    }
+                ?> 
+                <h1 class="display-4"><?php echo $header; ?></h1>
+                <p class="lead"><?php echo $message; ?></p>
+            </div>
+        </div>
     </body>
 </html>
