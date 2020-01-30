8e5697ac7ff2b4632709bcf57d033c208bfe6e97
Alexander Klee <aklee@pdc-online.com>
2020-01-30 14:06:14 +0100

    configure allowed file types

--- lib/tweakch/archive/filesystem_archive.php
+++ lib/tweakch/archive/filesystem_archive.php
@@ -11,14 +11,15 @@ include __DIR__ . '/memory_archive.php';
 class FileSystemArchive extends ArchiveBase  {
     // Properties
     public $max_size;
+    public $allowed_files;
 
     function __construct($config_path = null) {
         parent::__construct($config_path);
         $this->load_from_config('max_size',$this->max_size,500000);
+        $this->load_from_config('file_types', $this->allowed_files, "jpg,png,jpeg,gif");
     }
 
-    function add_file($file) {
-        
+    function add_file($file) {        
         // Check if image file is a actual image or fake image
         if(isset($_POST["submit"])) {
             $check = getimagesize($file["tmp_name"]);
@@ -33,9 +34,10 @@ class FileSystemArchive extends ArchiveBase  {
         }
 
         // Allow certain file formats
-        $imageFileType = strtolower(pathinfo($file["name"],PATHINFO_EXTENSION));
-        if($imageFileType != "jpg" && $imageFileType != "png" && $imageFileType != "jpeg" && $imageFileType != "gif" ) {
-            throw new Exception("Sorry, only JPG, JPEG, PNG & GIF files are allowed.");
+        $ext = strtolower(pathinfo($file["name"],PATHINFO_EXTENSION));
+        $allowed = isset(split(",",$this->allowed_files)[$ext]);
+        if(!$allowed) {
+            throw new Exception("Sorry, only {$this->allowed_files} files are allowed.");
         }
 
         # find a free path in the archive
@@ -45,9 +47,7 @@ class FileSystemArchive extends ArchiveBase  {
 
         // Create the directory if it does not exist
         $target_path = substr($target_file,0,strripos($target_file,"/"));
-        if(!is_dir($target_path)){
-            mkdir($target_path,0750,true);    
-        }
+        if(!is_dir($target_path)) mkdir($target_path,0750,true);    
 
         if (!move_uploaded_file($file["tmp_name"], $target_file)) {
             throw new Exception("Sorry, there was an error uploading your file.");
